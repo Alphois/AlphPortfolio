922facefddc0c689db0e4ea354c38d709188a8cd
Alphois <74471676+Alphois@users.noreply.github.com>
2023-05-29 12:48:16 +0000

    add siteProps

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Krishna Dudani",
+  title: "College Student in CS",
+  email: "krishna.dudani94@qmail.cuny.edu",
+  gitHub: "https://github.com/Alphois/",
+  instagram: "alphois_",
+  linkedIn: "https://www.linkedin.com/in/krishna-dudani-a28842216/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
