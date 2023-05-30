8521a148f3b23868568f51ed27279d51370791d5
Alphois <74471676+Alphois@users.noreply.github.com>
2023-05-30 01:43:12 +0000

    fix birb img

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,11 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-<<<<<<< HEAD
-import image from "../images/bird.jpg";
-=======
 import image from "../images/birb.jpg";
->>>>>>> 4272828 (better site)
 
 const imageAltText = "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
 
@@ -44,8 +40,8 @@ const Home = ({ name, title }) => {
 };
 
 Home.defaultProps = {
-  name: "",
-  title: "",
+  name: "Krishna Dudani",
+  title: "College Student",
 };
 
 Home.propTypes = {
