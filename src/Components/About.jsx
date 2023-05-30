07ded1052fc2fa89b8a62ea456509ad5c77a49d1
Alphois <74471676+Alphois@users.noreply.github.com>
2023-05-30 01:21:25 +0000

    Add skillList and About

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a first year student at Queens College moving into my second year. I like coding as a hobby. ";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "CSS",
+  "HTML",
+  "Javascript",
+  "Python",
+  "C++",
+  "Java"
 ];
 
 /**
@@ -44,8 +44,7 @@ const skillsList = [
  * how you best work, or even a quote. This will help someone learn more
  * about you on a professional level.
  */
-const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+const detailOrQuote ="Currently leaning into learning data science";
 
 const About = () => {
   return (
